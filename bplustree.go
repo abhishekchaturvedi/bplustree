897783cbc86dae9872cefcfcf7c7c6e7da675867
Abhishek Chaturvedi <abhishek.chatur@gmail.com>
2020-09-12 00:10:23 -0700

    Add functionality to insert
    This change consists of changes to implement the insert functionality
    for the bplustree.
    
    Tests are also added.

--- bplustree.go
+++ bplustree.go
@@ -4,7 +4,9 @@ import (
 	"errors"
 	"sync"
 	"fmt"
-	// "reflect"
+	"sort"
+	"os"
+	"io"
 )
 
 // Tentative errors. Will add more.
@@ -12,6 +14,7 @@ var (
 	ERR_NOT_FOUND = errors.New("key not found")
 	ERR_NOT_INITIALIZED = errors.New("Tree is not initialized")
 	ERR_INVALID_PARAM = errors.New("Invalid configuration parameter")
+	ERR_EXISTS = errors.New("Already exists")
 )
 
 // This is the locker interface. The clients of the library should fill in the
@@ -21,18 +24,24 @@ var (
 // Each of the lock/unlock calls are going to be used for Insert/Delete/Lookup
 // operations on the Tree.
 type BplusTreeLocker interface {
+	Init()
 	Lock(rkey []BplusTreeKey, wkey []BplusTreeKey)
 	Unlock(rkey []BplusTreeKey, wkey []BplusTreeKey)
 }
 
 // A default locker implementation using sync.RWMutex.
 type BplusTreeDefaultLock struct {
-	mux sync.RWMutex
+	mux *sync.RWMutex
+}
+func (lck *BplusTreeDefaultLock) Init() {
+	lck.mux = &sync.RWMutex{}
 }
 func (lck *BplusTreeDefaultLock) Lock (rkey []BplusTreeKey, wkey []BplusTreeKey) {
+	fmt.Printf("Taking lock.. for %v\n", wkey[0])
 	lck.mux.Lock()
 }
 func (lck *BplusTreeDefaultLock) Unlock (rkey []BplusTreeKey, wkey []BplusTreeKey) {
+	fmt.Printf("Unlocking now.... for %v\n", wkey[0])
 	lck.mux.Unlock()
 }
 
@@ -58,10 +67,8 @@ type BplusTreeDBMgr interface {
 // this BplusTree library to use user provided interface for allocation. The memory
 // manager could have it's own internal policy on how much to cache etc.
 // 'Insert' is the fucntion that will be called when a key is inserted to the tree.
-// 'Delete' is the function that will be called when a key is deleted from the tree.
 type BplusTreeMemMgr interface {
-	Insert(k []BplusTreeKey, v []BplusTreeElem) error
-	Delete(k []BplusTreeKey) error
+	Alloc() error
 }
 
 // The BplusTreeCtx struct defines the optional and necessary user-defined functions
@@ -109,21 +116,19 @@ type BplusTreeElem interface {
 	GetKey() BplusTreeKey
 }
 
+type BplusTreeElems []BplusTreeElem
 
 // The 'node' of the bplus tree. Each node is represented by the following data.'
-// 'children' is the list of children that this node has.
-// 'parent' is the pointer to the parent node.
+// 'children' is the list of children that this node has if not leaf node, otherwise
+//            it contains the content.
 // 'next' is the pointer to the next node (sibling to the right)
 // 'prev' is the poitner to the prev node (sibling to the lef).
 // 'isLeaf' whether this node is a leaf or not.
-// 'content' is the 'value' part of this node which is valid only if this node is a leaf
 type BplusTreeNode struct {
-	children	[]*BplusTreeNode
-	parent		*BplusTreeNode
+	children	BplusTreeElems
 	next		*BplusTreeNode
 	prev		*BplusTreeNode
 	isLeaf           bool
-	content	        *BplusTreeElem
 }
 
 type BplusTreeSearchDirection int
@@ -168,9 +173,290 @@ type BplusTreeSearchSpecifier struct {
 }
 
 
+// Non instance functions.
+func BplusTreeDefaultAlloc() *BplusTreeNode {
+	return &BplusTreeNode{}
+}
 func BplusTreeIsEmptyInterface(x interface{}) bool {
 	return x == nil
 }
+
+
+// BplusTreeElems instance functions.
+func (elems BplusTreeElems) find(key BplusTreeKey) (index int) {
+	index = sort.Search(len(elems), func(i int) bool {
+		ret := elems[i].GetKey().Compare(key)
+		return ret >= 0
+	})
+	return
+}
+
+func (elems BplusTreeElems) insert(elem BplusTreeElem, maxDegree int) (BplusTreeElems, error) {
+	index := elems.find(elem.GetKey())
+	fmt.Printf("found element's insert position at %d, (len: %d)\n", index, len(elems))
+	// Insert at the end case.
+	if index >= len(elems) {
+		fmt.Printf("Appending %v at %d\n", elem, index)
+		elems = append(elems, elem)
+		return elems, nil
+	}
+
+	// If inserting in the middle.
+	// XXX: Will need to use memMgr here.
+	fmt.Printf("Inserting %v at %d, increasing elems to (size: %d, cap: %d)\n", elem, index, len(elems) + 1, maxDegree + 1)
+	newElems := make(BplusTreeElems, len(elems) + 1, maxDegree + 1)
+	copy(newElems, elems[:index])
+	newElems[index] = elem
+	copy(newElems[index + 1:], elems[index:])
+	return newElems, nil
+}
+
+func (elems BplusTreeElems) String() string {
+	var elemStr []string
+	for _, elem := range elems {
+		switch elemType := elem.(type) {
+		case *BplusTreeNode:
+			elemStr = append(elemStr, fmt.Sprintf(" N <%v> ", elemType.GetKey()))
+		default:
+			elemStr = append(elemStr, fmt.Sprintf(" L <%v> ", elemType.GetKey()))
+		}
+	}
+	return fmt.Sprintf("%v", elemStr)
+}
+
+
+// BplusTreeNode instance functions.
+func (node *BplusTreeNode) insertElement(elem BplusTreeElem, maxDegree int) error {
+	children, err := node.children.insert(elem, maxDegree)
+	if err != nil {
+		return err
+	}
+
+	node.children = children
+	return nil
+}
+
+func (node *BplusTreeNode) GetKey() BplusTreeKey {
+	var tmpNode *BplusTreeNode = node
+	for !tmpNode.isLeaf {
+		tmpNode = tmpNode.children[0].(*BplusTreeNode)
+	}
+	return tmpNode.children[0].GetKey()
+}
+
+func (node *BplusTreeNode) String() string {
+	var prevKey, nextKey string
+	if node.prev != nil {
+		prevKey = fmt.Sprintf("%v", node.prev.GetKey())
+	} else {
+		prevKey = "nil"
+	}
+
+	if node.next != nil {
+		nextKey = fmt.Sprintf("%v", node.next.GetKey())
+	} else {
+		nextKey = "nil"
+	}
+
+	return fmt.Sprintf("{%p [children: %v, prev: %v, next: %v, leaf: %v]}",
+		node, node.children, prevKey, nextKey, node.isLeaf)
+}
+
+
+// BplusTree instance functions.
+func (bpt *BplusTree) BplusTreeNodeInit(
+	node *BplusTreeNode,
+	isLeaf bool,
+	next *BplusTreeNode,
+	prev *BplusTreeNode,
+	initLen int) {
+
+	node.children = make([]BplusTreeElem, initLen, bpt.context.maxDegree)
+	node.isLeaf = isLeaf
+	node.next = next
+	node.prev = prev
+}
+
+func (bpt *BplusTree) insertFinder(key BplusTreeKey) (nodes []*BplusTreeNode, err error) {
+	return bpt.find(key, func (node *BplusTreeNode, index int) (int, error) {
+		index -= 1
+		if index <= 0 {
+			index = 0
+		}
+		return index, nil
+
+	})
+}
+
+func (bpt *BplusTree) find(key BplusTreeKey, finder func(*BplusTreeNode, int) (int, error)) (nodes []*BplusTreeNode, err error) {
+	nodes = make([]*BplusTreeNode, 0) // We don't know the capacity.
+	node := bpt.root
+	if node == nil {
+		return nil, ERR_NOT_FOUND
+	}
+
+	for node != nil {
+		nodes = append(nodes, node)
+		if node.isLeaf {
+			break
+		}
+
+		cnodes := node.children
+
+		index := sort.Search(len(cnodes), func(i int) bool {
+			ret := cnodes[i].GetKey().Compare(key)
+			return ret >= 0 // Every key past this point is >=0
+		})
+
+		index, err = finder(node, index)
+		if err != nil {
+			fmt.Printf("encountered err: %s\n", err)
+			return
+		}
+		node = cnodes[index].(*BplusTreeNode)
+
+	}
+	return
+}
+
+func (bpt *BplusTree) rebalance(nodes []*BplusTreeNode) (err error) {
+	numNodes := len(nodes)
+
+	var parent, curr, next *BplusTreeNode
+
+	switch {
+	case numNodes == 1:
+		curr = nodes[0]
+		// XXX use memMgr.
+		parent = BplusTreeDefaultAlloc()
+		bpt.BplusTreeNodeInit(parent, false, nil, nil, 0)
+		parent.children = append(parent.children, curr)
+		// Make the new node the parent, and everything else accumulated
+		// so far becomes the children.
+		bpt.root = parent
+	default:
+		parent = nodes[numNodes - 2]
+		curr = nodes[numNodes - 1]
+	}
+
+	currChildren := curr.children
+	midp := len(currChildren) / 2
+
+	// XXX: Use memMgr.
+	next = BplusTreeDefaultAlloc()
+	bpt.BplusTreeNodeInit(next, curr.isLeaf, curr.next, curr, len(currChildren) - midp)
+	curr.children = currChildren[:midp]
+	copy(next.children, currChildren[midp:])
+	curr.next = next
+
+	if next.next != nil {
+		next.next.prev = next
+	}
+
+	err = parent.insertElement(next, bpt.context.maxDegree)
+	return
+}
+
+// Check to see if any rebalance is needed on the nodes traversed through insertion
+// of a new element
+func (bpt *BplusTree) checkRebalance(nodes []*BplusTreeNode) (err error) {
+	// Traverse in reverse order to address the nodes towards leaves first.
+	for i := len(nodes) - 1; i >= 0; i-- {
+		node := nodes[i]
+		degree := bpt.context.maxDegree
+		// // Leaf nodes are allowed 1 degree more.
+		// if node.isLeaf {
+		// 	degree = bpt.context.maxDegree + 1
+		// }
+		if len(node.children) > degree {
+			fmt.Printf("node %v (leaf: %v) has degree (%d) more than allowed, splitting\n",
+				node, node.isLeaf, len(node.children))
+			err = bpt.rebalance(nodes[:i+1])
+		} else {
+			fmt.Printf("node %v (leaf: %v) has degree (%d). Allowed\n",
+				node, node.isLeaf, len(node.children))
+		}
+	}
+	return
+}
+
+
+func (bpt *BplusTree) WriteTreeLayout(writer io.Writer) {
+	leafIdx := 0
+	nodeIdx := 0
+	levelIdx := 0
+
+	fmt.Fprintf(writer, "Dumping the tree layout.. \n")
+	nodeList := bpt.root.children
+	nodeLensList := make([]int, 1)
+	nodeLensList[0] = len(bpt.root.children)
+	numElems := nodeLensList[0]
+	for i := 0; i < numElems; i++ {
+		node := nodeList[i]
+		switch elemType := node.(type) {
+		case *BplusTreeNode:
+			if elemType.isLeaf {
+				fmt.Fprintf(writer, "<tree-L-node :%d, level: %d, node: %v> ",
+					leafIdx, levelIdx, elemType)
+				leafIdx += 1
+			} else {
+				fmt.Fprintf(writer, "<tree-I-node :%d, levelIdx: %d, node: %v> ",
+					nodeIdx, levelIdx, elemType)
+				nodeList = append(nodeList, elemType.children...)
+				nodeLensList = append(nodeLensList, len(elemType.children))
+				numElems += len(elemType.children)
+			}
+		default:
+			fmt.Fprintf(writer, "<elem-node :%d, level: %d, node: %v> ",
+				nodeIdx, levelIdx, elemType)
+		}
+		nodeIdx += 1
+		if nodeIdx >= nodeLensList[levelIdx] {
+			levelIdx += 1
+			nodeIdx = 0
+			fmt.Fprintf(writer, "\n")
+		}
+	}
+	fmt.Fprintf(writer, "Done dumping the tree layout\n")
+	fmt.Fprintf(writer, "----------------------------\n")
+}
+func (bpt *BplusTree) WriteTree(writer io.Writer, printLayout bool) error {
+	node := bpt.root
+	// Print tree layout.
+	if printLayout == true {
+		bpt.WriteTreeLayout(writer)
+	}
+
+	// Go to the left most leaf node and start printing in order.
+	for node != nil {
+		if node.isLeaf {
+			break
+		}
+		node = node.children[0].(*BplusTreeNode)
+	}
+
+	if node == nil {
+		fmt.Fprintln(writer, "Tree is empty")
+		return nil
+	}
+
+	index := 0
+	for node != nil {
+		fmt.Fprintf(writer, "leaf node: %d\n", index)
+		for _, child := range node.children {
+			fmt.Fprintf(writer, "\t%v\n", child)
+		}
+
+		node = node.next
+		index += 1
+	}
+	return nil
+}
+
+//
+// External functions.
+//
+
 
 // Create an instance of new tree. order and ctx specify the required parameters.
 // Returns pointer to the tree if successful, appropriate error otherwise.
@@ -182,21 +468,74 @@ func NewBplusTree(ctx BplusTreeCtx) (*BplusTree, error) {
 		return nil, ERR_INVALID_PARAM
 	}
 
+	// XXX: Use memory mgr to alloc.
+	// XXX: Use dbMgr to load to initialize.
 	bplustree := &BplusTree {root: nil, initialized: true, context: ctx}
 
 	if BplusTreeIsEmptyInterface(ctx.lockMgr) {
 		fmt.Printf("No locker specified. Using default locker using mutex\n")
 		bplustree.context.lockMgr = new(BplusTreeDefaultLock)
+		bplustree.context.lockMgr.Init()
 	}
 
 	return bplustree, nil
 }
+
 func (bpt *BplusTree) Insert (elem BplusTreeElem) error {
-	return nil
+	if !bpt.initialized {
+		return ERR_NOT_INITIALIZED
+	}
+
+	keys := make([]BplusTreeKey, 1)
+	keys[0] = elem.GetKey()
+	bpt.context.lockMgr.Lock(nil, keys)
+	defer bpt.context.lockMgr.Unlock(nil, keys)
+
+	fmt.Printf("Inserting %v\n", elem)
+
+	if bpt.root == nil {
+		newnode := BplusTreeDefaultAlloc()
+		bpt.BplusTreeNodeInit(newnode, true, nil, nil, 0)
+		newnode.children = append(newnode.children, elem)
+		bpt.root = newnode
+		fmt.Printf("Done..... Inserting %v\n", elem)
+		return nil
+	}
+
+	nodes, err := bpt.insertFinder(elem.GetKey())
+	if err != nil {
+		fmt.Printf("Inserting %v encountered error: %v\n", elem, err)
+		return err
+	}
+
+	fmt.Printf("Accumulated following nodes\n")
+	for _, n := range(nodes) {
+		fmt.Printf("<%v> ", n)
+	}
+	fmt.Printf("\n----------------------------\n")
+
+	nodeToInsertAt := nodes[len(nodes) - 1]
+	err = nodeToInsertAt.insertElement(elem, bpt.context.maxDegree)
+	if err != nil {
+		fmt.Printf("Inserting %v encountered error: %v\n", elem, err)
+		return err
+	}
+
+	err = bpt.checkRebalance(nodes)
+	fmt.Printf("Done..... Inserting %v. Printing...\n", elem)
+	bpt.Print()
+	fmt.Printf("Done..... Printing after insert of %v\n", elem)
+	return err
 }
+
 func (bpt *BplusTree) Remove(key BplusTreeKey) error {
 	return nil
 }
+
 func (bpt *BplusTree) Search(ss BplusTreeSearchSpecifier) error {
 	return nil
+}
+
+func (bpt *BplusTree) Print() error {
+	return bpt.WriteTree(os.Stdout, true)
 }
--- bplustree_test.go
+++ bplustree_test.go
@@ -1,9 +1,9 @@
 package bplustree
 
 import (
-	// "fmt"
+	"fmt"
 	"testing"
-	// "time"
+	"sync"
 )
 
 type TestingKey int
@@ -17,15 +17,65 @@ type TestingElem struct {
 	val int
 }
 
-func (elem *TestingElem) GetKey () BplusTreeKey {
+func (elem *TestingElem) GetKey() BplusTreeKey {
 	return BplusTreeKey(elem.key)
 }
 
+func (elem *TestingElem) String() string {
+	return fmt.Sprintf("key: %d, val: %d", elem.key, elem.val)
+}
+
+func (key *TestingKey) String() string {
+	return fmt.Sprintf("key: %d", key)
+}
+
+
+var newtree *BplusTree
+var numElems int = 12000
+var maxDegree int = numElems / 10
+
 func TestInit(t *testing.T) {
-	ctx := BplusTreeCtx{lockMgr: nil, maxDegree: 10}
-	_, err := NewBplusTree(ctx)
+	ctx := BplusTreeCtx{lockMgr: nil, maxDegree: maxDegree}
+	var err error
+	newtree, err = NewBplusTree(ctx)
 	if err != nil {
 		t.Errorf("Failed to create new bplustree: %v", err)
 		t.FailNow()
 	}
+}
+
+func InsertRoutine(t *testing.T, elem *TestingElem, errors []error, index int, wg *sync.WaitGroup)  {
+	defer wg.Done()
+	t.Logf("Inserting: %v", elem)
+	err := newtree.Insert(elem)
+	errors[index] = err
+	if err == nil {
+		//newtree.Print()
+	}
+	t.Logf("++++++++++++++++++++++++++++++++")
+}
+
+func TestInsert(t *testing.T) {
+	t.Log("Starting insert test..")
+	elems := make([]TestingElem, numElems)
+	errors := make([]error, numElems)
+	var wg sync.WaitGroup
+
+	for i := 0; i < numElems; i++ {
+		elem := &elems[i]
+		elem.key = TestingKey(i)
+		elem.val = i + 10
+		wg.Add(1)
+		go InsertRoutine(t, elem, errors, i, &wg)
+	}
+
+	wg.Wait()
+	for i := 0; i < numElems; i++ {
+		if errors[i] != nil {
+			t.Errorf("Failed to insert: %d: %v", i, errors[i])
+			t.FailNow()
+		}
+	}
+	t.Logf("\nFinal tree:\n")
+	newtree.Print()
 }
